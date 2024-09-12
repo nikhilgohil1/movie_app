bd68712975eb8b1966c29e34a276ef429a332db2
Nikhil Gohil <nikhil.gohil@unicoconnect.com>
2024-09-12 14:51:59 +0530

    Filled-Track-Style Progress Bar added

--- lib/api/constants.dart
+++ lib/api/constants.dart
@@ -1,4 +1,4 @@
-const apiKey = "deebada4982a77671b5784eaefdc7b06";
+const apiKey = "1843025b2bae031291477a0a2a8ce5e1";
 const upcomingApiUrlWithKey = "https://api.themoviedb.org/3/movie/upcoming?api_key=$apiKey";
 const popularApiUrlWithKey = "https://api.themoviedb.org/3/movie/popular?api_key=$apiKey";
 const topRatedApiUrlWithKey = "https://api.themoviedb.org/3/movie/top_rated?api_key=$apiKey";
