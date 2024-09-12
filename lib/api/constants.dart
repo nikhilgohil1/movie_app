d2b0195b63f85fc6325c4644c6ac18a7f841d20e
Nikhil Gohil <nikhil.gohil@unicoconnect.com>
2024-09-12 15:09:55 +0530

    Hiding API key

--- lib/api/constants.dart
+++ lib/api/constants.dart
@@ -1,4 +1,5 @@
-const apiKey = "1843025b2bae031291477a0a2a8ce5e1";
+import 'key.dart';
+
 const upcomingApiUrlWithKey = "https://api.themoviedb.org/3/movie/upcoming?api_key=$apiKey";
 const popularApiUrlWithKey = "https://api.themoviedb.org/3/movie/popular?api_key=$apiKey";
 const topRatedApiUrlWithKey = "https://api.themoviedb.org/3/movie/top_rated?api_key=$apiKey";
